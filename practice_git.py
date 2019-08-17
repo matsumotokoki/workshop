0263bbf0616669ec8bb5d84ad35769f1a920966e
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2019-08-18 01:24:59 +0900

    add todo

--- practice_git.py
+++ practice_git.py
@@ -2,6 +2,9 @@
 print("hello git\n")
 
 for i in range(10):
-    print("No."+str(i+1))
+    print("NO."+str(i+1))
+    for j in range(3):
+        #TODO add action
+        pass
 
 print("\ngood bye!")
